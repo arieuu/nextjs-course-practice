0361f553ee1f0126ea8da3d3995de1636b8f76bf
Pliskin <arieljanickcarvalho@gmail.com>
2024-01-22 09:56:44 -0100

    Finished this (first part) of course

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto } from 'next/font/google'
 import NavBar from './NavBar'
 import AuthProvider from './auth/Provider'
 
-const inter = Inter({ subsets: ['latin'] })
+// Setting up fonts
+
+const inter = Inter({ subsets: ['latin'] });
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ["400", "500"]
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,7 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="winter">
-      <body className={inter.className}>
+      <body className={roboto.className}>
 
         <AuthProvider>
           <NavBar />
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 import ProductCard from './components/ProductCard'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './api/auth/[...nextauth]/route';
+import vogue from "@/public/images/vogue.png";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   return (
     <main>
-      <h1> Hello { session && <span> {session.user!.name } </span>} </h1>
-      <ProductCard />
+      <h1> Hello World </h1>
     </main> 
   )
 }
